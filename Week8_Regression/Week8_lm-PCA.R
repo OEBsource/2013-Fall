c5f579c7348d78c930a4a1fcc801231fef67b003
AndrewJohnSmith <andy_j_smith@hotmail.co.uk>
2013-11-01 11:31:03 -0400

    Regression and PCA.

--- Week8_Regression/Week8_lm-PCA.R
+++ Week8_Regression/Week8_lm-PCA.R
@@ -1,3 +1,6 @@
+####SOURCE####
+#Week 8 - Regression lines and PCA#
+
 rm(list=ls())
 setwd("/Users/Home/Dropbox/Amherst/Courses/SOURCE/2013-Fall/Week8_Regression")
 
@@ -8,24 +11,38 @@ Fit<-lm(Anolis$hostility~Anolis$attitude)
 abline(Fit)
 
 str(Fit)
+#Find the residuals and view them.
 Fit$residuals
 
 plot(Anolis$attitude, Anolis$residuals, col=unclass(Anolis$Island), pch=20)
+#Plots a horizontal line at 0.
 abline(h=0)
+#Could also plot a vertical line at 0 using...
+#abline(v=0)
+
+#If you want to specify the colour of the plot use...
+#Colour<-c("red", "blue", "dark green", "black")
+#Then add col=Colour[unclass(Anolis$Island)] into the plot function.
 
 
-#PCA using bird wing data
+##Principle Component Analysis (PCA) using bird wing data.
 Birds<-read.csv("BirdWings.csv", header=T, row.names=1)
 head(Birds)
 str(Birds)
 
-#Birds.order <- Birds[order(Birds$ecotype),]
+#In some cases you may want to order your data by a factor.
+ #That's not required here but it would look like this.
+ #Birds.order <- Birds[order(Birds$ecotype),]
 
-WingPCA <- princomp(Birds, cor=TRUE)
-#Need to run without the factors
+#There are two different ways to run PCA.
+ #princomp and prcomp - apparently prcomp is preferred.
+ #I have seen very little difference between the two however.
+
+WingPCA <- prcomp(Birds, cor=TRUE)
+#Need to run without the factors.
 
 ncol(Birds)
-WingPCA <- princomp(Birds[ ,1:8], cor=T)
+WingPCA <- prcomp(Birds[ ,1:8], cor=T)
 #Cor=T means base on a correlation matrix
 #Cor=F means base on a covariation matrix
  #Which one you choose depends on the scale of your data.
@@ -33,41 +50,57 @@ WingPCA <- princomp(Birds[ ,1:8], cor=T)
 #covariance matrix when the variable scales are similar and the correlation matrix when variables are on different scales.
 #Using the correlation matrix standardises the data.
 
-summary(WingPCA) # print variance accounted for 
+#Print variance accounted for.
+summary(WingPCA)
 
-loadings(WingPCA) # pc loadings 
+#PC loadings
+loadings(WingPCA) 
 #First PC normally has a size component (unless from a landmark analysis)
 
-WingPCA$scores #View the principal components
+#View the principal components
+WingPCA$scores
+
+#Plot them as part of a morphospace
 plot(WingPCA$scores)
 
-#biplot(WingPCA)
 
-
+#I want to see how the different birds are arranged in morphospace.
+ #To do this I will plot them and group them by their flying style, or ecotype.
 
 WScores<-as.data.frame(WingPCA$scores)
+#We need to do this as a matrix will not allow additional entries to be added.
 WScores$ecotype<-Birds$ecotype
-class()
+class(WScores)
+#Good, it's now a data frame.
 
+#Let's plot the scores out.
 plot(WScores[,1], WScores[,2], pch=19, col=unclass(WScores$ecotype))
 
 #Subset our data into the two groups
 Glide <- subset(WScores, ecotype=="Glide")
 Raptor<- subset(WScores, ecotype=="Raptor")
 
-glide.hull <- chull(Glide)
-glide.hull <- c(glide.hull, glide.hull[1])
-lines(Glide[glide.hull, ], lty=1, col="black")
+#I want to see a convex hull surround the two groups.
+ #This will allow me to better visualize how much morphospace they occupy.
 
 #The convex hull plotting works by finding the points on the outside of the morphospace
 #It works using part of the polygon function.
 #You then use the lines function to plot it.
+glide.hull <- chull(Glide)
+#glide.hull now contains all the individuals on the outer edges of the morphospace.
+glide.hull <- c(glide.hull, glide.hull[1])
+#I add the first number back on using "c" so it forms a complete polygon.
+lines(Glide[glide.hull, ], lty=1, col="black")
+#I plot the lines by indexing the Glide rows by my boundary individuals.
+
 
 raptor.hull <- chull(Raptor)
 raptor.hull <- c(raptor.hull, raptor.hull[1])
 lines(Raptor[raptor.hull, ], lty=2, col="red")
 
+#Use this to plot the individual names onto your plots.
 text(WScores[,1], WScores[,2], row.names(WScores), cex=0.6, pos=4, col="red")
+#Change "pos=" to different numbers and the text will be placed in different positions on the plot.
 
 
 
@@ -75,20 +108,15 @@ text(WScores[,1], WScores[,2], row.names(WScores), cex=0.6, pos=4, col="red")
 #Read in BatData.csv - calcuate a PCA and plot the scores onto a morphospace.
 #Check the data - should it be a correlation or covaraition matrix?
 
-
-
-
 Bats<-read.csv("BatData.csv", header=T, row.names=1)
 head(Bats)
 
 ncol(Bats)
 BatPCA <- princomp(Bats[ ,1:4], cor=F)
 
-summary(BatPCA) # print variance accounted for 
+summary(BatPCA)
 
-loadings(BatPCA) # pc loadings 
-#First PC normally has a size component (unless from a landmark analysis)
+loadings(BatPCA)
 
-BatPCA$scores #View the principal components
+BatPCA$scores
 plot(BatPCA$scores)
-
