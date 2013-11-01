2b3b153bd31c80f76421ae631c2b1c99e35862b2
AndrewJohnSmith <andy_j_smith@hotmail.co.uk>
2013-11-01 09:42:36 -0400

    Regression and PCA script.

--- Week8_Regression/Week8_lm-PCA.R
+++ Week8_Regression/Week8_lm-PCA.R
@@ -1,45 +1,37 @@
 rm(list=ls())
 setwd("/Users/Home/Dropbox/Amherst/Courses/SOURCE/2013-Fall/Week8_Regression")
 
-Test<-read.table("Force_Displacement_2.txt", header=T, sep="\t")
+Anolis<-read.csv("anolisData.csv", header=T, na.strings="?")
 
-plot(Test$Displacement.um, Test$Force.Plastic.A.mn)
-fit<-lm(Force.Plastic.A.mn~Displacement.um, data=Test)
-abline(fit)
-summary(fit)
-
-str(fit)
-fit$residuals
-
-Test<-read.csv("anolisData.csv", header=T, na.strings="?")
-
-plot(Test$attitude, Test$hostility)
-Fit<-lm(Test$hostility~Test$attitude)
+plot(Anolis$attitude, Anolis$hostility)
+Fit<-lm(Anolis$hostility~Anolis$attitude)
 abline(Fit)
 
 str(Fit)
 Fit$residuals
 
-plot(Test$attitude, Fit$residuals, col=unclass(Test$Island), pch=20)
+plot(Anolis$attitude, Anolis$residuals, col=unclass(Anolis$Island), pch=20)
 abline(h=0)
 
 
-#PCA
+#PCA using bird wing data
 Birds<-read.csv("BirdWings.csv", header=T, row.names=1)
 head(Birds)
 str(Birds)
 
-Birds.order <- Birds[order(Birds$ecotype),]
+#Birds.order <- Birds[order(Birds$ecotype),]
 
-WingPCA <- princomp(Birds.order, cor=TRUE)
+WingPCA <- princomp(Birds, cor=TRUE)
 #Need to run without the factors
 
 ncol(Birds)
-WingPCA <- princomp(Birds.order[ ,1:8], cor=TRUE)
+WingPCA <- princomp(Birds[ ,1:8], cor=T)
 #Cor=T means base on a correlation matrix
 #Cor=F means base on a covariation matrix
  #Which one you choose depends on the scale of your data.
-  #If you have orders of magnitude differences between data - use
+  #If you have orders of magnitude differences between data - use correlation.
+#covariance matrix when the variable scales are similar and the correlation matrix when variables are on different scales.
+#Using the correlation matrix standardises the data.
 
 summary(WingPCA) # print variance accounted for 
 
@@ -55,19 +47,29 @@ plot(WingPCA$scores)
 
 
 WScores<-as.data.frame(WingPCA$scores)
-WScores$ecotype<-Birds.order$ecotype
+WScores$ecotype<-Birds$ecotype
 class()
 
 plot(WScores[,1], WScores[,2], pch=19, col=unclass(WScores$ecotype))
 
-glide.hull <- chull(WScores[1:8,])
+#Subset our data into the two groups
+Glide <- subset(WScores, ecotype=="Glide")
+Raptor<- subset(WScores, ecotype=="Raptor")
+
+glide.hull <- chull(Glide)
 glide.hull <- c(glide.hull, glide.hull[1])
-lines(WScores[1:8,][glide.hull, ], lty=1, col="black")
+lines(Glide[glide.hull, ], lty=1, col="black")
+
 #The convex hull plotting works by finding the points on the outside of the morphospace
 #It works using part of the polygon function.
 #You then use the lines function to plot it.
 
-raptor.hull <- chull(WScores[9:19,])
+raptor.hull <- chull(Raptor)
 raptor.hull <- c(raptor.hull, raptor.hull[1])
-lines(WScores[9:19,][raptor.hull,], lty=2, col="red")
+lines(Raptor[raptor.hull, ], lty=2, col="red")
 
+text(WScores[,1], WScores[,2], row.names(WScores), cex=0.6, pos=4, col="red")
+
+
+
+#PCO
